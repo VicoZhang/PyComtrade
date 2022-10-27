f2a68de18282acb7959a888910000d08f8db7b91
Vico Zhang <1248130720@qq.com>
2022-10-27 20:13:56 +0800

    版本2.0，增添了与matlab的接口，生成.mat文件用于数据交互

--- src/PyComtrade.py
+++ src/PyComtrade.py
@@ -3,13 +3,16 @@ import os.path
 import struct
 import csv
 import matplotlib
+import pinyin
 import matplotlib.pyplot as plt
 from PIL import Image
+from scipy.io import savemat
 
 
 class ComtradeFile:
 
     def __init__(self, file_path, file_name):
+        self.file_name = file_name
         self.cfg_path = os.path.join(file_path, file_name + '.cfg')
         self.dat_path = os.path.join(file_path, file_name + '.dat')
 
@@ -46,18 +49,18 @@ class ComtradeFile:
         print('通道数量解析错误') if self.a_chan_num + self.d_chan_num != self.channel_num else ...
 
         # 频率信息
-        self.sys_freq = eval(self.cfg_info[2+self.channel_num][0])
+        self.sys_freq = eval(self.cfg_info[2 + self.channel_num][0])
         print('注意系统频率非50Hz') if self.sys_freq != 50 else ...
 
         # 采样信息
-        self.sample_freq_num = eval(self.cfg_info[3+self.channel_num][0])
-        self.sample_freq = [eval(self.cfg_info[4+self.channel_num+i][0]) for i in range(0, self.sample_freq_num)]
-        self.sample_num = [eval(self.cfg_info[4+self.channel_num+i][1]) for i in range(0, self.sample_freq_num)]
+        self.sample_freq_num = eval(self.cfg_info[3 + self.channel_num][0])
+        self.sample_freq = [eval(self.cfg_info[4 + self.channel_num + i][0]) for i in range(0, self.sample_freq_num)]
+        self.sample_num = [eval(self.cfg_info[4 + self.channel_num + i][1]) for i in range(0, self.sample_freq_num)]
         ... if self.sample_freq_num == len(self.sample_freq) and self.sample_freq_num == len(
             self.sample_num) else print('采样信息处读取错误')
 
         # 数据存储信息
-        self.data_format = self.cfg_info[6+self.channel_num+self.sample_freq_num][0]
+        self.data_format = self.cfg_info[6 + self.channel_num + self.sample_freq_num][0]
 
         # 模拟量含义
         for i in range(self.a_chan_num):
@@ -83,7 +86,7 @@ class ComtradeFile:
         self.dat_read_length = 4 + 4 + 2 * self.a_chan_num + 2 * math.ceil(self.d_chan_num / 16)  # 单次解析数据字节长度
         self.pack_format = '<' + 'I' * 2 + 'h' * self.a_chan_num + 'H' * math.ceil(self.d_chan_num / 16)  # 单词解析数据类型
         n_struct = struct.Struct(self.pack_format)
-        mode = 'rb' if self.data_format == 'BINARY' else 'r'  # todo 有待考究
+        mode = 'rb' if self.data_format == 'BINARY' else 'r'
 
         with open(self.dat_path, mode=mode) as dat_file:
             self.a_data_info = [dat_file.read(self.dat_read_length) for _ in range(self.sample_num[-1])]
@@ -123,19 +126,33 @@ class ComtradeFile:
             plt.close()
         print("图片已经正常绘制，保存在{}目录下".format(path))
 
+    def save_mat(self, path):
+        os.makedirs(path) if not os.path.exists(path) else ...
+        mat_file = path + '/{}.mat'.format(self.file_name)
+        try:
+            savemat(mat_file, {'{}'.format(self.a_chan_info[i]['a_name']): self.a_chan_info[i]['ana_data'] for i in
+                               range(self.a_chan_num)})
+            print("mat数据文件已生成，保存在{}目录下".format(path))
+        except UnicodeEncodeError:
+            savemat(mat_file,
+                    {'{}'.format(pinyin.get(self.a_chan_info[i]['a_name'], format="numerical"))
+                     : self.a_chan_info[i]['ana_data'] for i in range(self.a_chan_num)})
+            print("mat数据文件已生成，保存在{}目录下"
+                  "\n注意！由于变量名中包含汉字，无法在MATLAB中对其命名，故转换为拼英命名"
+                  "\n命名规则参见https://pypi.org/project/pinyin/".format(path))
+
 
 if __name__ == '__main__':
-    test1 = ComtradeFile('../test_file/2022-09-14_04-08-18_110kV全安站_10kV F17线路保护RCS-9611B_录波文件', '2022-09-14-04_08_18-039-00019-00028')
-    test1.save_csv('../csv')
+    # test1 = ComtradeFile('../test_file/2022-09-14_04-08-18_110kV全安站_10kV F17线路保护RCS-9611B_录波文件', '2022-09-14-04_08_18-039-00019-00028')
     # test2 = ComtradeFile('../test_file/2022-09-16_21-16-52_35kV枇杷岭站A机_10kV石下线CSC-211_录波文件', 'PL10_62_RCD_136_20220916_211652_803')
     # test3 = ComtradeFile('../test_file/2022-09-18_21-29-45_110kV全安站_10kV F29线路保护RCS-9611B_录波文件', '2022-09-18-21_29_45-613-00043-00002')
     # test4 = ComtradeFile('../test_file/2022-09-23_16-53-08_110kV全安站_10kV F26线路保护RCS-9611B_录波文件', '2022-09-23-16_53_08-293-00040-34599')
     # test5 = ComtradeFile('../test_file/2022-09-24_08-07-10_110kV全安站_10kV F17线路保护RCS-9611B_录波文件', '2022-09-24-08_07_10-991-00019-00032')
     # test6 = ComtradeFile('../test_file/2022-09-24_08-17-28_110kV横江站_10kV F20馈线_录波文件', 'D35_RCD_00144_20220924_081728_521')
-    # test7 = ComtradeFile('../test_file/2022-09-25_18-30-46_110kV横江站_10kV F33馈线_录波文件', 'D44_RCD_00045_20220925_183046_779')
-    # test8 = ComtradeFile('../test_file', 'BAY01_0001_20221020_114531_483')
+    # test7 = ComtradeFile('../test_file/2022-09-25_18-30-46_110kV横江站_10kV F33馈线_录波文件','D44_RCD_00045_20220925_183046_779')
+    test8 = ComtradeFile('../test_file', 'BAY01_0001_20221020_114531_483')
     # test9 = ComtradeFile('../test_file', 'BAY01_0001_20221020_114520_483')
+    test8.save_mat('../mat')
     # test10 = ComtradeFile('../test_file', 'cfg_file')
     # test11 = ComtradeFile('../test_file', '2018年05月09日15时23分34秒')
     print()
-
